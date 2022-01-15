9104608d886f600f820c588c7150686877ca833f
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2022-01-15 14:27:42 +0000

    Added levels.

--- src/commands/economy/profile.ts
+++ src/commands/economy/profile.ts
@@ -51,7 +51,8 @@ export class Profile extends Command {
           color: COLORS[profile.color],
           thumbnail: { url: user.avatarURL },
           fields: [
-            { name: `📛 ` + this.client.locale.translate(data.locale, 'economy.REPUTATION'), value: profile.reputation, inline: true },
+            { name: '📛 ' + this.client.locale.translate(data.locale, 'economy.REPUTATION'), value: profile.reputation, inline: true },
+            { name: '💡 ' + this.client.locale.translate(data.locale, 'economy.GLOBAL_LEVEL'), value: profile.level.toString(), inline: true },
             { name: '💖 ' + this.client.locale.translate(data.locale, 'economy.LOVER'), value: profile.lover ? `<@${profile.lover}>` : this.client.locale.translate(data.locale, 'economy.SINGLE'), inline: true },
             { name: '⚧ ' + this.client.locale.translate(data.locale, 'economy.GENDER'), value: profile.gender, inline: true }
           ]
