9a23870c7adf2c79afdb007081efcfd6237599d8
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2022-02-08 21:24:32 +0000

    Cleaned up isSuperior.

--- src/utilities/ModerationUtilities.ts
+++ src/utilities/ModerationUtilities.ts
@@ -8,11 +8,11 @@
 import type { Member, Role } from 'eris';
 
 // Misc functions
-const getTopRole = (member: Member): Role | undefined => {
-  return member.roles.map(r => member.guild.roles.get(r)).sort((a, b) => b!.position - a!.position)[0];
+const getTopRole = (member: Member): Role => {
+  return member.roles.map(r => member.guild.roles.get(r)).sort((a, b) => b!.position - a!.position)[0] ?? ({ position: -1 } as Role);
 };
 
 // Export functions
 export const isSuperior = (memberOne: Member, memberTwo: Member): boolean => {
-  return memberOne.guild.ownerID === memberOne.id ?? (getTopRole(memberOne)?.position ?? -1) > (getTopRole(memberTwo)?.position ?? -1);
+  return memberOne.guild.ownerID === memberOne.id ?? getTopRole(memberOne).position > getTopRole(memberTwo).position;
 };
