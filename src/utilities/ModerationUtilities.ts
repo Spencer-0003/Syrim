42f37f3b38741b2835a6db0b5fbd526a713d484d
Spencer-0003 <36741900+Spencer-0003@users.noreply.github.com>
2022-03-30 15:33:50 +0100

    Fixed unnecessary assertions.

--- src/utilities/ModerationUtilities.ts
+++ src/utilities/ModerationUtilities.ts
@@ -9,7 +9,7 @@ import type { Member, Role } from 'eris';
 
 // Misc functions
 const getTopRole = (member: Member): Role | Partial<Role> => {
-  return member.roles.map(r => member.guild.roles.get(r)).sort((a, b) => b!.position - a!.position)[0] ?? { position: -1 };
+  return member.roles.map(r => member.guild.roles.get(r)!).sort((a, b) => b.position - a.position)[0] ?? { position: -1 };
 };
 
 // Export functions
