0114ce886e8ff60bd52d552e80a5a88f07da8ae6
Spencer-0003 <36741900+Spencer-0003@users.noreply.github.com>
2022-04-03 13:12:45 +0100

    Added sentry.release.

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@
 
 import 'module-alias/register';
 import '@utilities/ErisPatch';
-import { configureScope, init } from '@sentry/node';
+import { init } from '@sentry/node';
 import { SyrimClient } from '@core/Client';
 import { commitHash } from '@utilities/Constants';
 
-if (process.env.SENTRY_DSN) {
-  init({ dsn: process.env.SENTRY_DSN, environment: process.env.NODE_ENV, attachStacktrace: true });
-  configureScope(scope => scope.setTags({ 'syrim.commitHash': commitHash }));
-} else console.warn('Sentry DSN not found, no error reporting will be sent.');
+if (process.env.SENTRY_DSN) init({ dsn: process.env.SENTRY_DSN, environment: process.env.NODE_ENV, attachStacktrace: true, release: commitHash });
+else console.warn('Sentry DSN not found, no error reporting will be sent.');
 
 new SyrimClient(process.env.DISCORD_TOKEN).launch();
