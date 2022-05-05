07e737d9795c0b2c76c0ff9f7b8323034367c4f1
Spencer-0003 <36741900+Spencer-0003@users.noreply.github.com>
2022-05-05 02:03:54 +0100

    Debug cache error.

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -22,6 +22,7 @@ export class Database {
 
     this.prisma.$use(async (params, next) => {
       if (params.model !== 'Blacklist' || params.action !== 'findUnique') return next(params);
+      console.log(params);
 
       const key = Object.keys(params.args.select)[0];
       const cached = await this.redis.get(`blacklist:${key}`);
@@ -29,7 +30,7 @@ export class Database {
       if (process.env.NODE_ENV === 'development') {
         console.log(`keys: ${Object.keys(params.args.select)}`);
         console.log(`key: ${key}`);
-        console.log(`Cached result: ${cached}`)
+        console.log(`Cached result: ${cached}`);
       }
 
       if (cached) return JSON.parse(cached);
